ba8b3070f873eba026a79953bef358c3bbd5b9a8
Simon Rehwald <simon.rehwald@tum.de>
2018-12-23 15:18:40 +0100

    remove --dry-run

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 mkdir -p dist/css
 cp index.html dist/ && cp -r css/ dist/css
-rsync --dry-run -az --force --delete --progress -e "ssh -p22" ./dist/ $DEPLOY
+rsync -az --force --delete --progress -e "ssh -p22" ./dist/ $DEPLOY

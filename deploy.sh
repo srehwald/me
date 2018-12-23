ac23542c1f3848fa8b64245a5d95faa46c6eee47
Simon Rehwald <simon.rehwald@tum.de>
2018-12-23 15:22:59 +0100

    deploy htaccess

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p dist/css
-cp index.html dist/ && cp -r css/ dist/css
+cp index.html dist/ && cp .htaccess dist/ && cp -r css/ dist/css/
 rsync -az --force --delete --progress -e "ssh -p22" ./dist/ $DEPLOY
